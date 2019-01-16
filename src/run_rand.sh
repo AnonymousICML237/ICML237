5bab2a9de3e17cf4edf8ace336fad54157ab614c
AnonymousICML237 <46703002+AnonymousICML237@users.noreply.github.com>
2019-01-16 09:51:19 +0800

    Create run_rand.sh

---
+++ src/run_rand.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+python rand.py \
+  --batch_size 128 \
+  --img_size 32 \
+  --img_resize 35 \
+  --iter_times 1 \
+  --num_classes 10 \
+  --test_data_path "../data_path/test_adv(eps_0.031).p" \
+  --test_label_path "../label_path/test_label.p" \
+  --model 'vgg' \
+  --model_path "../model_path/lat_param.pkl"
