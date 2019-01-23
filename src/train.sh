fecca8c1381d988feeca41d2168ac5e50202c482
AnonymousICML237 <46703002+AnonymousICML237@users.noreply.github.com>
2019-01-23 22:48:46 +0800

    Update train.sh

--- src/train.sh
+++ src/train.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo 'training model...'
-python dplat.py --batchsize 128 --lr 0.0001 --epoch 40 --test_flag 'False' --enable_lat 'False' --pro_num 5 --alpha 0.7 --epsilon 0.3 --model_path "/media/dsg3/dsgprivate/lat/model/vgg/" --test_data_path "/media/dsg3/dsgprivate/lat/test/new/test_data_cln.p" --test_label_path "/media/dsg3/dsgprivate/lat/test/new/test_label.p" --train_data_path "/media/dsg3/dsgprivate/lat/data/cifar10/" --dataset 'cifar10' --model 'vgg' --dropout 'True' 
+python anp.py --batchsize 128 --lr 0.0001 --epoch 40 --test_flag 'False' --enable_lat 'False' --pro_num 5 --alpha 0.7 --epsilon 0.3 --model_path "/media/dsg3/dsgprivate/lat/model/vgg/" --test_data_path "./test_data_cln.p" --test_label_path "./test_label.p" --train_data_path "./data/cifar10/" --dataset 'cifar10' --model 'vgg' --dropout 'True' 
